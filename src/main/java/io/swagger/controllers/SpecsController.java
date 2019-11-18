bdb3b1123b452f34f32b69c062587de268efd0ff
Tony Tam <tony@eatbacon.org>
2019-11-18 08:01:55 -0800

    better logging

--- src/main/java/io/swagger/controllers/SpecsController.java
+++ src/main/java/io/swagger/controllers/SpecsController.java
@@ -1,8 +1,12 @@
 package io.swagger.controllers;
 
 import com.google.common.io.Files;
+import io.swagger.oas.inflector.controllers.OpenAPIOperationController;
 import io.swagger.oas.inflector.models.RequestContext;
 import io.swagger.oas.inflector.models.ResponseContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.core.Response.Status;
 
 import java.io.File;
@@ -10,16 +14,20 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Date;
 import java.util.stream.Collectors;
 
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaInflectorServerCodegen", date = "2019-11-16T17:17:40.998Z[GMT]")
+public class SpecsController  {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpecsController.class);
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaInflectorServerCodegen", date = "2019-11-16T17:17:40.998Z[GMT]")public class SpecsController  {
-    public static String SPEC_FILE = "src/main/swagger/openapi.yaml";
+    public static String TARGET_SPEC_FILE = "src/main/swagger/openapi.yaml";
     public ResponseContext getSpecification(RequestContext request ) {
         try {
-            File file = new File(SPEC_FILE);
+            LOGGER.debug("trying to load file");
+            File file = new File(TARGET_SPEC_FILE);
             if(!file.exists()) {
+                LOGGER.debug("no file found, loading default from resource");
                 file = new File(
                     getClass().getClassLoader().getResource("default.yaml").getFile()
                 );
@@ -39,18 +47,18 @@ import java.util.stream.Collectors;
     }
 
     public ResponseContext saveSpecification(RequestContext request, String body ) {
-        File file = new File(SPEC_FILE);
+        File file = new File(TARGET_SPEC_FILE);
 
         try {
             if(!file.exists()) {
-                String[] dirs = SPEC_FILE.split("/");
+                String[] dirs = TARGET_SPEC_FILE.split("/");
                 dirs = Arrays.copyOf(dirs, dirs.length-1);
                 File directory = new File(String.join("/", dirs));
-                System.out.println(directory.getAbsolutePath());
+                LOGGER.info("created directory `" + directory.getAbsolutePath() + "`");
                 directory.mkdirs();
             }
-            Files.write(body.getBytes(), Paths.get(SPEC_FILE).toFile());
-            System.out.println("saved spec");
+            Files.write(body.getBytes(), Paths.get(TARGET_SPEC_FILE).toFile());
+            LOGGER.info("saved spec to `" + TARGET_SPEC_FILE + "`");
             return new ResponseContext().status(201);
         }
         catch(IOException e) {
